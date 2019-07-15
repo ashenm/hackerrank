83d2ed3e6857cbb07d22c7b138c48b68e4f35dce
Ashen Gunaratne <mail@ashenm.ml>
2019-07-15 09:22:25 +0000

    Add completions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # HackerRank #
 
 ## Linux SHell ##
+- [A Personalized Echo](challenges/bash-tutorials-a-personalized-echo.sh)
 - [Let's Echo](challenges/bash-tutorials-lets-echo.sh)
 - [Looping and Skipping](challenges/bash-tutorials-looping-and-skipping.sh)
 
---
+++ challenges/bash-tutorials-a-personalized-echo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+#
+# A Personalized Echo
+# Print a customised welcome message
+# https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem
+#
+# Ashen Gunaratne
+# mail@ashenm.ml
+#
+
+# prompt input
+read NAME
+
+# stdout customised greet
+echo "Welcome ${NAME}"
